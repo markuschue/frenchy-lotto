268348b7fadc0ed492004596aa8266bbafeabd12
Markus <markus@portato.home>
2023-11-12 17:50:47 +0000

    add lottoutils

--- src/types/LottoType.ts
+++ src/types/LottoType.ts
@@ -1,5 +1,10 @@
 import { lottoTypes } from "../assets/data.json";
 
 type LottoType = (typeof lottoTypes)[number];
+export type LottoResult = {
+	numbers: number[];
+	stars: number[];
+	numComp: number;
+};
 
 export default LottoType;
---
+++ src/utils/lottoUtils.ts
@@ -0,0 +1,88 @@
+import { LottoResult } from "../types/LottoType";
+
+const getRandomIntInclusive = (min: number, max: number) => {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+export const powerBall = () => {
+	const PBresult: LottoResult = {
+		numbers: [],
+		stars: [],
+		numComp: 0,
+	};
+	for (let i = 0; i < 5; i++) {
+		PBresult.numbers.push(Math.floor(getRandomIntInclusive(1, 69)));
+	}
+	PBresult.stars.push(Math.floor(getRandomIntInclusive(1, 26)));
+	return PBresult;
+};
+
+export const megaMillions = () => {
+	const MMresult: LottoResult = {
+		numbers: [],
+		stars: [],
+		numComp: 0,
+	};
+	for (let i = 0; i < 5; i++) {
+		MMresult.numbers.push(Math.floor(getRandomIntInclusive(1, 70)));
+	}
+	MMresult.stars.push(Math.floor(getRandomIntInclusive(1, 25)));
+	return MMresult;
+};
+
+export const euroMillions = () => {
+	const EMresult: LottoResult = {
+		numbers: [],
+		stars: [],
+		numComp: 0,
+	};
+	for (let i = 0; i < 5; i++) {
+		EMresult.numbers.push(Math.floor(getRandomIntInclusive(1, 50)));
+	}
+	for (let i = 0; i < 2; i++) {
+		EMresult.stars.push(Math.floor(getRandomIntInclusive(1, 12)));
+	}
+	return EMresult;
+};
+
+export const euroJackpot = () => {
+	const EJresult: LottoResult = {
+		numbers: [],
+		stars: [],
+		numComp: 0,
+	};
+	for (let i = 0; i < 5; i++) {
+		EJresult.numbers.push(Math.floor(getRandomIntInclusive(1, 50)));
+	}
+	for (let i = 0; i < 2; i++) {
+		EJresult.stars.push(Math.floor(getRandomIntInclusive(1, 12)));
+	}
+	return EJresult;
+};
+
+export const primitva = () => {
+	const Presult: LottoResult = {
+		numbers: [],
+		stars: [],
+		numComp: 0,
+	};
+	for (let i = 0; i < 6; i++) {
+		Presult.numbers.push(Math.floor(getRandomIntInclusive(1, 49)));
+	}
+	Presult.stars.push(Math.floor(getRandomIntInclusive(0, 9)));
+	return Presult;
+};
+
+export const bonnoLotto = () => {
+	const BLresult: LottoResult = {
+		numbers: [],
+		stars: [],
+		numComp: 0,
+	};
+	for (let i = 0; i < 6; i++) {
+		BLresult.numbers.push(Math.floor(getRandomIntInclusive(1, 49)));
+	}
+	return BLresult;
+};
